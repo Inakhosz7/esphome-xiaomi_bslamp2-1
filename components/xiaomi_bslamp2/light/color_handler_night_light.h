7d12f37243ff6f93a8619bd9eff71a7f4aa9b69e
Gerber Zoltán <skylc015@skylc.local>
2022-02-16 20:36:28 +0100

    Update color_handler_night_light.h

--- components/xiaomi_bslamp2/light/color_handler_night_light.h
+++ components/xiaomi_bslamp2/light/color_handler_night_light.h
@@ -40,7 +40,7 @@ class ColorHandlerNightLight : public ColorHandler {
     if (v.get_color_mode() == light::ColorMode::COLOR_TEMPERATURE) {
     // red = 0.968f; 
       red = 0.962f; //temperature
-      green = 0.972f;
+      green = 0.950f;
       blue = 0.962f;
       white = 0.0f;
     }
